acaf3d219cf045e3c636801aaeabaa737ea75b70
Mohammed Faheem <immohammedfaheem@gmail.com>
2023-08-21 20:41:23 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from torchinfo import summary
 from prettytable import PrettyTable
 
+
 # Print a Comprehensive Summary of the Model, Modules, Submodules, Parameter Counts
 def model_summary(model, generator)
 review_batch, label, mask_batch = next(generator)
@@ -20,3 +21,44 @@ def count_parameters(model):
     print(table)
     print(f"Total Trainable Params: {total_params}")
     return total_params
+
+
+# plot the training loss and training, testing accuracy
+def plot_metrics(num_epochs, batch_per_epoch_train, batch_per_epoch_train, train_loss, train_acc, test_acc):
+
+    # X axis Epoch
+    train_epochs = list(range(1, (batch_per_epoch_train * num_epochs) + 1))
+    test_epochs = list(range(1, (batch_per_epoch_train * num_epochs) + 1))
+    
+    # Create subplots for train loss and accuracy
+    plt.figure(figsize=(24, 3))
+    
+    # Plot Train Loss
+    plt.plot(train_epochs, train_loss, label='Train Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training Loss')
+    plt.legend()
+    
+    # Plot Train Accuracy
+    plt.figure(figsize=(24, 3))
+    # plt.subplot(1, 2, 2)
+    plt.plot(train_epochs, train_acc, label='Train Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy')
+    plt.legend()
+    
+    # Plot Test Accuracy
+    plt.figure(figsize=(24, 3))
+    plt.plot(test_epochs, test_acc, label='Test Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Testing Accuracy')
+    plt.legend()
+    
+    # Adjust layout
+    #plt.tight_layout()
+    
+    # Show the plots
+    plt.show()
